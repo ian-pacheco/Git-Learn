4faedffbe75e62e123e7f6c3418894aa3bf533bd
Ian Pacheco <ianpacheco64@gmail.com>
2018-06-19 14:36:12 -0300

    Projeto iniciado

---
+++ teste/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace teste {
+    class Program {
+        static void Main(string[] args) {
+
+            double largura, comprimento, PrecoMetro, area, preco;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            PrecoMetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * PrecoMetro;
+
+            Console.WriteLine("Área: "+ area);
+            Console.WriteLine("Preço: "+ PrecoMetro);
+
+        }
+    }
+}
